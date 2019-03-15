516a5e31186c1683b3c5ec559195c5c4b2883942
Oskar Björk <Oskar.DTK.Bjork@lystkom.se>
2019-03-15 13:33:51 +0100

    Add project files.

---
+++ chat/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace chat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnServerStart = new System.Windows.Forms.Button();
+            this.edtServerPort = new System.Windows.Forms.TextBox();
+            this.edtServerIP = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnClientStart = new System.Windows.Forms.Button();
+            this.edtClientIP = new System.Windows.Forms.TextBox();
+            this.edtClientPort = new System.Windows.Forms.TextBox();
+            this.redtHistory = new System.Windows.Forms.RichTextBox();
+            this.edtToSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblP2 = new System.Windows.Forms.Label();
+            this.lblP1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnServerStart);
+            this.groupBox1.Controls.Add(this.edtServerPort);
+            this.groupBox1.Controls.Add(this.edtServerIP);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(988, 90);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(644, 58);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "5000->";
+            // 
+            // btnServerStart
+            // 
+            this.btnServerStart.Location = new System.Drawing.Point(785, 25);
+            this.btnServerStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnServerStart.Name = "btnServerStart";
+            this.btnServerStart.Size = new System.Drawing.Size(100, 28);
+            this.btnServerStart.TabIndex = 2;
+            this.btnServerStart.Text = "Start";
+            this.btnServerStart.UseVisualStyleBackColor = true;
+            this.btnServerStart.Click += new System.EventHandler(this.btnServerStart_Click);
+            // 
+            // edtServerPort
+            // 
+            this.edtServerPort.Location = new System.Drawing.Point(644, 25);
+            this.edtServerPort.Margin = new System.Windows.Forms.Padding(4);
+            this.edtServerPort.Name = "edtServerPort";
+            this.edtServerPort.Size = new System.Drawing.Size(132, 22);
+            this.edtServerPort.TabIndex = 1;
+            // 
+            // edtServerIP
+            // 
+            this.edtServerIP.Location = new System.Drawing.Point(24, 25);
+            this.edtServerIP.Margin = new System.Windows.Forms.Padding(4);
+            this.edtServerIP.Name = "edtServerIP";
+            this.edtServerIP.Size = new System.Drawing.Size(611, 22);
+            this.edtServerIP.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnClientStart);
+            this.groupBox2.Controls.Add(this.edtClientIP);
+            this.groupBox2.Controls.Add(this.edtClientPort);
+            this.groupBox2.Location = new System.Drawing.Point(16, 165);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(988, 92);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Client";
+            // 
+            // btnClientStart
+            // 
+            this.btnClientStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClientStart.Location = new System.Drawing.Point(785, 31);
+            this.btnClientStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClientStart.Name = "btnClientStart";
+            this.btnClientStart.Size = new System.Drawing.Size(100, 28);
+            this.btnClientStart.TabIndex = 3;
+            this.btnClientStart.Text = "Connect";
+            this.btnClientStart.UseVisualStyleBackColor = true;
+            this.btnClientStart.Click += new System.EventHandler(this.btnClientStart_Click);
+            // 
+            // edtClientIP
+            // 
+            this.edtClientIP.Location = new System.Drawing.Point(24, 33);
+            this.edtClientIP.Margin = new System.Windows.Forms.Padding(4);
+            this.edtClientIP.Name = "edtClientIP";
+            this.edtClientIP.Size = new System.Drawing.Size(611, 22);
+            this.edtClientIP.TabIndex = 2;
+            // 
+            // edtClientPort
+            // 
+            this.edtClientPort.Location = new System.Drawing.Point(644, 33);
+            this.edtClientPort.Margin = new System.Windows.Forms.Padding(4);
+            this.edtClientPort.Name = "edtClientPort";
+            this.edtClientPort.Size = new System.Drawing.Size(132, 22);
+            this.edtClientPort.TabIndex = 1;
+            // 
+            // redtHistory
+            // 
+            this.redtHistory.Location = new System.Drawing.Point(40, 265);
+            this.redtHistory.Margin = new System.Windows.Forms.Padding(4);
+            this.redtHistory.Name = "redtHistory";
+            this.redtHistory.Size = new System.Drawing.Size(752, 117);
+            this.redtHistory.TabIndex = 2;
+            this.redtHistory.Text = "";
+            // 
+            // edtToSend
+            // 
+            this.edtToSend.Location = new System.Drawing.Point(40, 390);
+            this.edtToSend.Margin = new System.Windows.Forms.Padding(4);
+            this.edtToSend.Name = "edtToSend";
+            this.edtToSend.Size = new System.Drawing.Size(611, 22);
+            this.edtToSend.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(692, 391);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(100, 28);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(801, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "P1 Score";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(202, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "P2 Score";
+            // 
+            // lblP2
+            // 
+            this.lblP2.AutoSize = true;
+            this.lblP2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblP2.Location = new System.Drawing.Point(316, 126);
+            this.lblP2.Name = "lblP2";
+            this.lblP2.Size = new System.Drawing.Size(23, 25);
+            this.lblP2.TabIndex = 8;
+            this.lblP2.Text = "0";
+            // 
+            // lblP1
+            // 
+            this.lblP1.AutoSize = true;
+            this.lblP1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblP1.Location = new System.Drawing.Point(151, 126);
+            this.lblP1.Name = "lblP1";
+            this.lblP1.Size = new System.Drawing.Size(23, 25);
+            this.lblP1.TabIndex = 9;
+            this.lblP1.Text = "0";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(856, 368);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 56);
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.Value = 1;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(851, 324);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.lblP1);
+            this.Controls.Add(this.lblP2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.edtToSend);
+            this.Controls.Add(this.redtHistory);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox edtServerPort;
+        private System.Windows.Forms.TextBox edtServerIP;
+        private System.Windows.Forms.TextBox edtClientIP;
+        private System.Windows.Forms.TextBox edtClientPort;
+        private System.Windows.Forms.RichTextBox redtHistory;
+        private System.Windows.Forms.TextBox edtToSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Button btnServerStart;
+        private System.Windows.Forms.Button btnClientStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblP2;
+        private System.Windows.Forms.Label lblP1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ chat/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace chat
+{
+    //Thx to "Csharp Tutorials" at https://youtu.be/X16IyNbcAr0
+
+    public partial class Form1 : Form
+    {
+        private TcpClient client; //Listens for connections from TCP network clients.
+        public StreamReader STR;
+        public StreamWriter STW;
+        public string recieve;
+        public string textToSend;
+        Dictionary<string, int> data = new Dictionary<string, int>();
+        string[] names_send = { "", "host:  ", "client:  " };
+        string[] names_recieve = { "", "client:  ", "host:  " };
+
+
+        // KNAS?
+        public Form1()
+        {
+            InitializeComponent();
+            //The name localhost normally resolves to the IPv4 loopback address 127.0.0.1, and to the IPv6 loopback address ::1
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress adress in localIP)
+            {
+                //Checks for ipV4
+                if (adress.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    edtServerIP.Text = adress.ToString();
+                }
+            }
+            data.Add("player1", 0);
+            data.Add("player2", 0);
+            data.Add("maxscore", 0);
+            data.Add("winner", 0);
+            data.Add("gamestate", 0);
+            data.Add("User", 0);
+            
+        }
+        
+        private void btnServerStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Check with ex Wireshark 
+                TcpListener listener = new TcpListener(IPAddress.Any, int.Parse(edtServerPort.Text));
+                listener.Start();
+                this.BackColor = Color.Green;
+                this.Update();
+                redtHistory. ("Server started" + "\n");
+                redtHistory.Update();
+                client = listener.AcceptTcpClient(); //Accept a pending connection request 
+                STR = new StreamReader(client.GetStream());
+                STW = new StreamWriter(client.GetStream());
+                STW.AutoFlush = true;
+                backgroundWorker1.RunWorkerAsync();
+                backgroundWorker2.WorkerSupportsCancellation = true;
+                data["user"] = 1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void btnClientStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new TcpClient();
+
+                //Represents a network endpoint as an IP address and a port number.
+                IPEndPoint ipEnd = new IPEndPoint(IPAddress.Parse(edtClientIP.Text), int.Parse(edtClientPort.Text));
+
+                //Connects the client to a remote TCP host using the specified host name and port number.
+                client.Connect(ipEnd);
+                if (client.Connected)
+                {
+                    this.BackColor = Color.Green;
+                    this.Update();
+                    redtHistory.AppendText("Connected to Server" + "\n");
+                    redtHistory.Update();
+                    STR = new StreamReader(client.GetStream());
+                    STW = new StreamWriter(client.GetStream());
+                    STW.AutoFlush = true;
+                    backgroundWorker1.RunWorkerAsync();
+                    backgroundWorker2.WorkerSupportsCancellation = true;
+                    data["user"] = 2;
+                    trackBar1.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    recieve = STR.ReadLine();
+                    if (recieve.StartsWith("start"))
+                    {
+                        string[] list = recieve.Split();
+                        start(list[1]);
+                    }
+                    else
+                    {
+                        this.redtHistory.Invoke(new MethodInvoker(delegate ()
+                        {
+                            redtHistory.AppendText(names_recieve[data["user"]] + recieve + "\n");
+                        }));
+                        if (data["gamestate"] == 1)
+                        {
+
+                        }
+                    }
+                    recieve = "";
+           
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (client.Connected)
+            {
+                STW.WriteLine(textToSend);
+                this.redtHistory.Invoke(new MethodInvoker(delegate ()
+                {
+                    redtHistory.AppendText(names_send[data["user"]] + textToSend + "\n");
+                }));
+                if(data["gamestate"] == 1)
+                {
+                    data["player1"]++;
+                    this.lblP1.Invoke(new MethodInvoker(delegate ()
+                    {
+                        lblP1.Text = data["player1"].ToString();
+                    }));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sending failed");
+            }
+            backgroundWorker2.CancelAsync();
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (edtToSend.Text != "")
+            {
+                textToSend = edtToSend.Text;
+                backgroundWorker2.RunWorkerAsync();
+            }
+            edtToSend.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int wordint = rnd.Next(0, 13);
+            string[] word = { "Albertopolis", "bingle", "clepsydra", "defervescence",
+                "divagate", "ergometer", "famulus", "futhark", "glaikit", "higgler", "humdudgeon",
+                "martlet", "nainsook", "orrery"};
+            textToSend = "start " + word[wordint];
+            backgroundWorker2.RunWorkerAsync();
+            start(word[wordint]);
+        }
+
+        private void start(string word)
+        {
+            if (data["winner"] == 0)
+            {
+                redtHistory.AppendText("5 \n");
+                redtHistory.Update();
+                System.Threading.Thread.Sleep(1000);
+                redtHistory.AppendText("4 \n");
+                redtHistory.Update();
+                System.Threading.Thread.Sleep(1000);
+                redtHistory.AppendText("3 \n");
+                redtHistory.Update();
+                System.Threading.Thread.Sleep(1000);
+                redtHistory.AppendText("2 \n");
+                redtHistory.Update();
+                System.Threading.Thread.Sleep(1000);
+                redtHistory.AppendText("1 \n");
+                redtHistory.Update();
+                System.Threading.Thread.Sleep(1000);
+                redtHistory.AppendText("0 \n");
+                redtHistory.Update();
+                System.Threading.Thread.Sleep(1000);
+
+                //redtHistory.Text = word;
+                data["gamestate"] = 1;
+
+            }
+            else
+            {
+                MessageBox.Show("Game is already over");
+            }
+
+        }
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            label4.Text = trackBar1.Value.ToString();
+            data["maxscore"] = trackBar1.Value;
+            redtHistory.AppendText("maxscore set to: " + data["maxscore"].ToString() + "\n");
+        }
+    }
+}
